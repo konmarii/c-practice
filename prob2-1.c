4b2051e110db2d7339eb13d08cbbf3dbd5f4ffef
konmarii <chankonworld@gmail.com>
2021-12-11 08:50:21 +0000

    prob2-1

--- prob2-1.c
+++ prob2-1.c
@@ -4,5 +4,15 @@ void main(){
     int a;
     printf("a=");
     scanf("%d", &a);
-    printf("aの値は%dです。\n", a);
+    
+    int b;
+    printf("b=");
+    scanf("%d", &b);
+
+    printf("a+b=%d\n", a+b);
+    printf("a-b=%d\n", a-b);
+    printf("a*b=%d\n", a*b);
+    printf("a/b=%d\n", a/b);
+    printf("a%%b=%d\n", a%b);
+    
 }
